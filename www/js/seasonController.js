c638627b8339eb3795ad9c0756bfa95f8485a3a3
iplay88keys <iplay88keys@users.noreply.github.com>
2016-05-08 20:46:58 -0600

    Episodes now are checked if watched

--- www/js/seasonController.js
+++ www/js/seasonController.js
@@ -13,9 +13,22 @@ showTracker.controller('seasonCtrl', function($scope, $http, $rootScope, $stateP
         $scope.episodes = response.data.episodes;
         $scope.info = response.data.info;
         $scope.extras = response.data.extras;
+        $scope.watches = response.data.watches;
+
+        $scope.checked = {};
+        
+        angular.forEach($scope.episodes, function(episode) {
+            $scope.checked[episode.id] = false;
+        });
+        
+        angular.forEach($scope.watches, function(watch) {
+            $scope.checked[watch] = true;
+        });
+
     }, function error(response) {
         $scope.seasons = response.statusText;
         $scope.info = response.statusText;
         $scope.extras = response.statusText;
+        $scope.watches = response.statusText;
     });
 }); 
